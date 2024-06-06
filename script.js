6bd73e74640811d462edfa99b7358684b672e97d
esjuanbravo <esjuanbravo12@gmail.com>
2024-06-05 23:06:48 -0400

    se añadió las primeras funciones de ventanas emergentes

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+// variables mobile
+const mobileLogoMenu = document.querySelector('.menu');
+const toggleMobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', intercambioMenu);
+//botones de mi orden y mi cuenta
+const myOrdens = document.querySelector('.my-orders');
+const myAccount = document.querySelector('.my-account');
 
-function intercambioMenu() {
+// variables del caarrito 
+const contaddorCarritoCompras = document.getElementsByName('a');
+const carritoCompras = document.querySelector('.carrito-icon');
+var numeroProducto = 0;
+
+//funionalidades en desktop
+menuEmail.addEventListener('click', toggleMenuDesktop);
+
+function toggleMenuDesktop() {
     desktopMenu.classList.toggle('inactive')
+}
+
+// funcionalidades en mobile
+mobileLogoMenu.addEventListener('click',toggleMenuMobile);
+
+function toggleMenuMobile() {
+    toggleMobileMenu.classList.toggle('inactive')
+    
 }
