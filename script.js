690844466a096642250ecb43af5e2525344b983d
esjuanbravo <esjuanbravo12@gmail.com>
2024-06-07 22:07:25 -0400

    main listo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// variables de menu derecho desktop 
+// variables de menu derecho desktop
 const menuEmail = document.querySelector('.navbar-email');
 const menuHamIcon = document.querySelector('.menu');
 
@@ -8,8 +8,10 @@ const desktopMenu = document.querySelector('.desktop-menu');
 //varibles para en menu izquierdo para mobile
 const mobileMenu = document.querySelector('.mobile-menu');
 const aside = document.querySelector('.product-detail');
+//variables para el contenedor
+const cardsContainer = document.querySelector('.cards-container');
 
-// declaracion de acciones 
+// declaracion de acciones
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -23,7 +25,7 @@ function toggleDesktopMenu() {
   if (!isAsideClosed) {
     aside.classList.add('inactive');
   }
-  
+
   desktopMenu.classList.toggle('inactive');
 }
 
@@ -32,22 +34,102 @@ function toggleMobileMenu() {
   const isAsideClosed = aside.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    aside.classList.add('inactive'); 
+    aside.classList.add('inactive');
   }
-  
+
   mobileMenu.classList.toggle('inactive');
 }
 
 // funciones del Aside
 function toggleCarritoAside() {
   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
-  
+
   if (!isMobileMenuClosed) {
-    mobileMenu.classList.add('inactive'); 
+    mobileMenu.classList.add('inactive');
   }else if (isMobileMenuClosed){
     desktopMenu.classList.add('inactive')
   }
 
-  
   aside.classList.toggle('inactive');
 }
+
+// se va a crear html en js con asignacion de valores a travez de objetos y ciclos
+const productList = [];
+
+productList.push ({
+    name:'Bike',
+    price: 12700,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 800,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 600,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+});
+
+function renderProduct(arr) {
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const priceAndName = document.createElement('div');
+
+    const price = document.createElement('p');
+    price.innerText ='$' + product.price;
+    const name = document.createElement('p');
+    name.innerText = product.name;
+
+    const figure = document.createElement('figure');
+    const imgIcon = document.createElement('img');
+    imgIcon.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    //añadiendo elementos a la esructura html en js
+    cardsContainer.append(productCard)
+    productCard.append(productImg, productInfo)
+    productInfo.append(priceAndName, figure)
+    figure.append(imgIcon)
+    priceAndName.append(price, name)
+    }
+}
+renderProduct(productList);
